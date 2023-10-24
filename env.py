88d7009ced79d5ca0eedb7bc6b02745bbe375794
Mehatab Pathan <mehatab@Mehatabs-MacBook-Air.local>
2023-10-24 12:26:53 +0100

    added enu.py file

--- env.py
+++ env.py
@@ -2,3 +2,4 @@ import os
 
 os.environ["DATABASE_URL"]="postgres://asfanmbn:CZaVKCBAvzvUXV__FCiZK3c6EWs76ao_@flora.db.elephantsql.com/asfanmbn"
 os.environ["SECRET_KEY"]="my_super^secret@key"
+os.environ["cloudinary://816261457542735:e2H-2zdWjTrFphGsOtAR58eAoDM@dgofyu2eb"]
